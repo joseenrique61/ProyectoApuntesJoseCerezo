75aad8ed4f8fdf26b5cbd36947da62c7bad8ae15
José Enrique Cerezo <joseenrique61@hotmail.com>
2024-11-28 21:05:07 -0500

    Terminado el proyecto

--- ProyectoApuntesJoseCerezo/Views/NotePage.xaml.cs
+++ ProyectoApuntesJoseCerezo/Views/NotePage.xaml.cs
@@ -1,29 +1,56 @@
 namespace ProyectoApuntesJoseCerezo.Views;
 
+[QueryProperty(nameof(ItemId), nameof(ItemId))]
 public partial class NotePage : ContentPage
 {
-	string _fileName = Path.Combine(FileSystem.AppDataDirectory, "notes.txt");
+	public string ItemId
+	{
+		set { LoadNote(value); }
+	}
 
 	public NotePage()
 	{
 		InitializeComponent();
 
-		if (File.Exists(_fileName))
-			TextEditor.Text = File.ReadAllText(_fileName);
+		string appDataPath = FileSystem.AppDataDirectory;
+		string randomFileName = $"{Path.GetRandomFileName()}.notes.txt";
+
+		LoadNote(Path.Combine(appDataPath, randomFileName));
 	}
 
-	private void SaveButton_Clicked(object sender, EventArgs e)
+	private void LoadNote(string fileName)
 	{
-		// Save the file.
-		File.WriteAllText(_fileName, TextEditor.Text);
+		Models.Note noteModel = new()
+		{
+			Filename = fileName
+		};
+
+		if (File.Exists(fileName))
+		{
+			noteModel.Date = File.GetCreationTime(fileName);
+			noteModel.Text = File.ReadAllText(fileName);
+		}
+
+		BindingContext = noteModel;
 	}
 
-	private void DeleteButton_Clicked(object sender, EventArgs e)
+	private async void SaveButton_Clicked(object sender, EventArgs e)
 	{
-		// Delete the file.
-		if (File.Exists(_fileName))
-			File.Delete(_fileName);
+		if (BindingContext is Models.Note note)
+			File.WriteAllText(note.Filename, TextEditor.Text);
 
-		TextEditor.Text = string.Empty;
+		await Shell.Current.GoToAsync("..");
+	}
+
+	private async void DeleteButton_Clicked(object sender, EventArgs e)
+	{
+		if (BindingContext is Models.Note note)
+		{
+			// Delete the file.
+			if (File.Exists(note.Filename))
+				File.Delete(note.Filename);
+		}
+
+		await Shell.Current.GoToAsync("..");
 	}
 }
