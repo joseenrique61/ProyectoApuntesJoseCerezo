974f106b41d0cda86e92c18d10a6ba40180de879
José Enrique Cerezo <joseenrique61@hotmail.com>
2024-12-19 20:13:44 -0500

    Cambiado allnotes a viewmodel

--- ProyectoApuntesJoseCerezo/ViewModels/JCNotesViewModel.cs
+++ ProyectoApuntesJoseCerezo/ViewModels/JCNotesViewModel.cs
@@ -1,12 +1,60 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CommunityToolkit.Mvvm.Input;
+using ProyectoApuntesJoseCerezo.Models;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
 
 namespace ProyectoApuntesJoseCerezo.ViewModels
 {
-	internal class JCNotesViewModel
+	internal class JCNotesViewModel : IQueryAttributable
 	{
+		public ObservableCollection<ViewModels.JCNoteViewModel> AllNotes { get; }
+
+		public ICommand NewCommand { get; }
+
+		public ICommand SelectNoteCommand { get; }
+
+		public JCNotesViewModel()
+		{
+			AllNotes = new ObservableCollection<ViewModels.JCNoteViewModel>(JCNote.LoadAll().Select(n => new JCNoteViewModel(n)));
+			NewCommand = new AsyncRelayCommand(NewNoteAsync);
+			SelectNoteCommand = new AsyncRelayCommand<ViewModels.JCNoteViewModel>(SelectNoteAsync);
+		}
+
+		private async Task NewNoteAsync()
+		{
+			await Shell.Current.GoToAsync(nameof(Views.NotePage));
+		}
+
+		private async Task SelectNoteAsync(ViewModels.JCNoteViewModel note)
+		{
+			if (note != null)
+				await Shell.Current.GoToAsync($"{nameof(Views.NotePage)}?load={note.Identifier}");
+		}
+
+		void IQueryAttributable.ApplyQueryAttributes(IDictionary<string, object> query)
+		{
+			if (query.ContainsKey("deleted"))
+			{
+				string noteId = query["deleted"].ToString();
+				JCNoteViewModel matchedNote = AllNotes.Where((n) => n.Identifier == noteId).FirstOrDefault();
+
+				// If note exists, delete it
+				if (matchedNote != null)
+					AllNotes.Remove(matchedNote);
+			}
+			else if (query.ContainsKey("saved"))
+			{
+				string noteId = query["saved"].ToString();
+				JCNoteViewModel matchedNote = AllNotes.Where((n) => n.Identifier == noteId).FirstOrDefault();
+
+				// If note is found, update it
+				if (matchedNote != null)
+					matchedNote.Reload();
+
+				// If note isn't found, it's new; add it.
+				else
+					AllNotes.Add(new JCNoteViewModel(JCNote.Load(noteId)));
+			}
+		}
 	}
 }
