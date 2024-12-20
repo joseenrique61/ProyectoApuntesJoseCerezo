f30d5df27ff4944ecf8819f71cebf5a060a504ef
José Enrique Cerezo <joseenrique61@hotmail.com>
2024-12-19 19:56:03 -0500

    Cambiado notepage a viewmodel

--- ProyectoApuntesJoseCerezo/Views/NotePage.xaml.cs
+++ ProyectoApuntesJoseCerezo/Views/NotePage.xaml.cs
@@ -1,56 +1,9 @@
 namespace ProyectoApuntesJoseCerezo.Views;
 
-[QueryProperty(nameof(ItemId), nameof(ItemId))]
 public partial class NotePage : ContentPage
 {
-	public string ItemId
-	{
-		set { LoadNote(value); }
-	}
-
 	public NotePage()
 	{
 		InitializeComponent();
-
-		string appDataPath = FileSystem.AppDataDirectory;
-		string randomFileName = $"{Path.GetRandomFileName()}.notes.txt";
-
-		LoadNote(Path.Combine(appDataPath, randomFileName));
-	}
-
-	private void LoadNote(string fileName)
-	{
-		Models.JCNote noteModel = new()
-		{
-			Filename = fileName
-		};
-
-		if (File.Exists(fileName))
-		{
-			noteModel.Date = File.GetCreationTime(fileName);
-			noteModel.Text = File.ReadAllText(fileName);
-		}
-
-		BindingContext = noteModel;
-	}
-
-	private async void SaveButton_Clicked(object sender, EventArgs e)
-	{
-		if (BindingContext is Models.JCNote note)
-			File.WriteAllText(note.Filename, TextEditor.Text);
-
-		await Shell.Current.GoToAsync("..");
-	}
-
-	private async void DeleteButton_Clicked(object sender, EventArgs e)
-	{
-		if (BindingContext is Models.JCNote note)
-		{
-			// Delete the file.
-			if (File.Exists(note.Filename))
-				File.Delete(note.Filename);
-		}
-
-		await Shell.Current.GoToAsync("..");
 	}
 }
